954c6206a6094a66afd6466389e950b27eb6ad46
Asfak Ahmed <asfakahmed680@gmail.com>
2024-03-30 22:24:33 +0600

    templates data added

--- src/Utils/TemplatesData.js
+++ src/Utils/TemplatesData.js
@@ -55,4 +55,85 @@ export const templatesData = [
     githubLink: "https://github.com/ZenUI-Templates/furniture-rtui",
     liveLink: "https://rtui-furniture.netlify.app/",
   },
+  {
+    title: "Crypto Marketplace",
+    description:
+      "Crypto Binance owns the worlds leading digital asset trading platform, meet your trading needs.",
+    image: "https://i.ibb.co/xg9W89g/Untitled-design-16.png",
+    githubLink: "https://github.com/ZenUI-Templates/NFT",
+    liveLink: "https://nft-zen-ui.vercel.app/",
+  },
+  {
+    title: "Property Fox",
+    description:
+      "Property Fox empowers advisors with the expertise to seamlessly transition their clients between mortgage products as circumstances and opportunities evolve.",
+    image: "https://i.ibb.co/dWnBTSV/Untitled-design-17.png",
+    githubLink: "https://github.com/ZenUI-Templates/Property_Fox_template",
+    liveLink: "https://rtui-propertyfox.netlify.app/",
+  },
+  {
+    title: "Plumb tech",
+    description:
+      "Welcome to our innovative online banking platform, where convenience meets security. With our cutting-edge technology and user-friendly interface, managing your finances has never been easier. Whether checking your balance, transferring funds, or paying bills,",
+    image: "https://i.ibb.co/c8JQWL0/Untitled-design-18.png",
+    githubLink: "https://github.com/ZenUI-Templates/rtui-plumb-tech",
+    liveLink: "https://rtui-plumb-services.netlify.app/",
+  },
+  {
+    title: "Sass landing page",
+    description:
+      "End-to-end payments and financial management in a single solution. Meet the right platform to help realize.",
+    image: "https://i.ibb.co/9GSrHt4/Untitled-design-19.png",
+    githubLink: "https://github.com/ZenUI-Templates/saas-landing-page-01",
+    liveLink: "https://saas-landing-page-zenui.netlify.app/",
+  },
+  {
+    title: "sunny cosmetics",
+    description:
+      "Maybelline’s #1 foundation perfect for oily skin. No stickiness, no shine.",
+    image: "https://i.ibb.co/wWgfMmJ/Untitled-design-20.png",
+    githubLink: "https://github.com/ZenUI-Templates/sunny-cosmetics",
+    liveLink: "https://rtui-sunny-cosmetics.netlify.app/",
+  },
+  {
+    title: "LOGIS platform",
+    description:
+      "Never at water me might. On formed merits hunted unable merely by mr whence or. Possession the unpleasing simplicity her uncommonly.",
+    image: "https://i.ibb.co/DWJ6hv5/Capture.png",
+    githubLink: "https://github.com/ZenUI-Templates/logis-sass-platform",
+    liveLink: "https://rtui-logis-sass-platform.netlify.app/",
+  },
+  {
+    title: "Business RUI",
+    description:
+      "Relation so in confined smallest children unpacked delicate. Why sir end believe uncivil respect. Always get adieus nature day course for common. My little garret repair to desire he esteem.",
+    image: "https://i.ibb.co/tD8TMzB/Untitled-design-22.png",
+    githubLink: "https://github.com/ZenUI-Templates/Business_RUI",
+    liveLink: "https://business-rui.vercel.app/",
+  },
+  {
+    title: "Properties For Rent",
+    description:
+      "Search over 2000 properties to rent from the top agents in the country",
+    image: "https://i.ibb.co/nBgfMt7/Untitled-design-23.png",
+    githubLink: "https://github.com/ZenUI-Templates/-rtui-project-real-state",
+    liveLink: "https://real-state-template-alpha.vercel.app/",
+  },
+  {
+    title: "Nine star agency",
+    description:
+      "React-Tailwind UI is a free platform where you get all the components and designs you need to use.",
+    image: "https://i.ibb.co/8byQbgW/Untitled-design-24.png",
+    githubLink: "https://github.com/ZenUI-Templates/nine-star-digital-agency",
+    liveLink: "https://rtui-nine-star-digital-agency.netlify.app/",
+  },
+  {
+    title: "Insight Verse",
+    description:
+      "Expand your web development knowledge with our tutorials and learning articles.",
+    image: "https://i.ibb.co/D4pBj5j/Untitled-design-25.png",
+    githubLink:
+      "https://github.com/ZenUI-Templates/react-tailwind-blog-template",
+    liveLink: "https://react-tailwind-blog-template.netlify.app/",
+  },
 ];
