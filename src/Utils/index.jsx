75f6bf3e30a736900f9e737fc26bf4d0999da9e1
asfak00 <asfakahmed680@gmail.com>
2024-07-29 00:20:05 +0600

    update

--- src/Utils/index.jsx
+++
@@ -1,8 +0,0 @@
-const utils = {
-  buttonPrimary:
-    "py-2 px-6 bg-primary border border-primary text-secondary rounded font-[500]",
-  buttonSecondary:
-    "buttonSecondary py-2 px-6 border border-primary text-primary rounded font-[500] relative overflow-hidden z-10",
-};
-
-export default utils;
